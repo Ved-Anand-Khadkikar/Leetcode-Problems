f713297048122ab25747c96a7e62dd618297140a
Ved-Anand-Khadkikar <130249839+Ved-Anand-Khadkikar@users.noreply.github.com>
2026-06-06 23:29:18 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.27%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int lowerBound(vector<int>& arr, int x) {
+        int n = arr.size();
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+        int first = -1, last = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    int upperBound(vector<int>& arr, int x) {
+        int n = arr.size();
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+        int first = -1, last = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int lb = lowerBound(nums, target);
+        int ub = upperBound(nums, target);
+        if (lb == nums.size() || nums[lb] != target) {
+            return {-1, -1};
+        }
+        return {lb, ub - 1};
+    }
+};
